30f0c6b92c39e89e9e82d70d036495f310b23b44
Patryk Stradomski <patrykstradomski@wp.pl>
2021-12-01 19:02:37 +0100

    Solved day 1, cleaned up after template.

---
+++ src/Advent.kt
@@ -0,0 +1,5 @@
+fun main() {
+//    val result = day01.part1()
+    val result = day01.part2()
+    println(result)
+}
---
+++ src/day01/SonarSweep.kt
@@ -0,0 +1,43 @@
+package day01
+
+import java.io.File
+
+/***
+ * https://adventofcode.com/2021/day/1
+ */
+private fun readMeasurements(): List<Int> {
+    val measurements = mutableListOf<Int>()
+    File("src/day01/input.txt")
+        .useLines { lines ->
+            lines.forEach {
+                measurements.add(it.toInt())
+            }
+        }
+    return measurements
+}
+
+fun part1(): Int {
+    val measurements = readMeasurements()
+    var numberOfIncreases = 0
+    var previousMeasurement = measurements.first()
+
+    for (measurement in measurements) {
+        if (measurement > previousMeasurement) numberOfIncreases++
+        previousMeasurement = measurement
+    }
+    return numberOfIncreases
+}
+
+fun part2(): Int {
+    val measurements = readMeasurements()
+    var numberOfIncreases = 0
+    var slidingWindow = measurements.subList(0, 2).sum()
+    var prevSlidingWindow: Int
+
+    for (i in 3 until measurements.size) {
+        prevSlidingWindow = slidingWindow
+        slidingWindow = slidingWindow + measurements[i] - measurements[i - 3]
+        if (slidingWindow > prevSlidingWindow) numberOfIncreases++
+    }
+    return numberOfIncreases
+}
